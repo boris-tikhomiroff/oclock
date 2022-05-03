8c4d242d01f6447f8bc8dafce9c7634ec6021bad
Boris <boris.tikhomiroff@laplateforme.io>
2022-05-03 15:49:24 +0200

    mise en place du front

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function animate() {
     if (toggle) {
-      y = lerp(y, 0, 0.055);
-      c = lerp(c, 0, 0.075);
+      y = lerp(y, 0, 0.035);
+      c = lerp(c, 0, 0.105);
       path.setAttribute(
         "d",
         `M 0 ${y} L 0 100 100 100 100 ${y} C ${50} ${c}, ${50} ${c}, 0 ${y}`
@@ -53,7 +53,70 @@ document.addEventListener("DOMContentLoaded", () => {
     menuToggle.classList.toggle("active");
   });
 
-    /* ------------------------------------------------
-                    BURGER MENU
+  /* ------------------------------------------------
+                    CURSOR
 --------------------------------------------------- */
+  const cursor = document.querySelector("#cursor");
+  const cursorCircle = cursor.querySelector(".cursor__circle");
+
+  const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
+  const pos = { x: 0, y: 0 }; // cursor's coordinates
+  const speed = 0.1; // between 0 and 1
+
+  const updateCoordinates = (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  };
+
+  window.addEventListener("mousemove", updateCoordinates);
+
+  function getAngle(diffX, diffY) {
+    return (Math.atan2(diffY, diffX) * 180) / Math.PI;
+  }
+
+  function getSqueeze(diffX, diffY) {
+    const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+    const maxSqueeze = 0.15;
+    const accelerator = 1500;
+    return Math.min(distance / accelerator, maxSqueeze);
+  }
+
+  const updateCursor = () => {
+    const diffX = Math.round(mouse.x - pos.x);
+    const diffY = Math.round(mouse.y - pos.y);
+
+    pos.x += diffX * speed;
+    pos.y += diffY * speed;
+
+    const angle = getAngle(diffX, diffY);
+    const squeeze = getSqueeze(diffX, diffY);
+
+    const scale = "scale(" + (1 + squeeze) + ", " + (1 - squeeze) + ")";
+    const rotate = "rotate(" + angle + "deg)";
+    const translate = "translate3d(" + pos.x + "px ," + pos.y + "px, 0)";
+
+    cursor.style.transform = translate;
+    cursorCircle.style.transform = rotate + scale;
+  };
+
+  function loop() {
+    updateCursor();
+    requestAnimationFrame(loop);
+  }
+
+  requestAnimationFrame(loop);
+
+  const cursorModifiers = document.querySelectorAll("[cursor-class]");
+
+  cursorModifiers.forEach((curosrModifier) => {
+    curosrModifier.addEventListener("mouseenter", function () {
+      const className = this.getAttribute("cursor-class");
+      cursor.classList.add(className);
+    });
+
+    curosrModifier.addEventListener("mouseleave", function () {
+      const className = this.getAttribute("cursor-class");
+      cursor.classList.remove(className);
+    });
+  });
 });
--- src/js/reveil.js
+++ src/js/reveil.js
@@ -1,88 +1,17 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const display = document.getElementById("clock");
-//   const clearButton = document.querySelector(".clear-alarm");
-//   const setButton = document.querySelector(".set-alarm");
-//   const userDate = document.querySelector("input[type=datetime-local]");
-//   let ul = document.querySelector("ul");
-//   const audio = new Audio(
-//     "https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3"
-//   );
-//   audio.loop = true;
-//   let alarmTime = null;
-//   let alarmTimeout = null;
-
-//   function updateTime() {
-//     const date = new Date();
-
-//     const hour = formatTime(date.getHours());
-//     const minutes = formatTime(date.getMinutes());
-//     const seconds = formatTime(date.getSeconds());
-
-//     display.innerText = `${hour} : ${minutes} : ${seconds}`;
-//   }
-
-//   function formatTime(time) {
-//     if (time < 10) {
-//       return "0" + time;
-//     }
-//     return time;
-//   }
-
-//   function setAlarmTime(value) {
-//     alarmTime = value;
-//   }
-
-//   function displayAlarm() {
-//     let li = document.createElement("li");
-//     li.appendChild(document.createTextNode(userDate.value));
-//     ul.appendChild(li);
-//   }
-
-//   function setAlarm() {
-//     if (alarmTime) {
-//       const current = new Date();
-//       const timeToAlarm = new Date(alarmTime);
-
-//       if (timeToAlarm > current) {
-//         const timeout = timeToAlarm.getTime() - current.getTime();
-//         alarmTimeout = setTimeout(() => audio.play(), timeout);
-//         alert("Alarm set");
-//         displayAlarm();
-//       }
-//     }
-//     console.log("setalarm");
-//   }
-
-//   function clearAlarm() {
-//     audio.pause();
-//     if (alarmTimeout) {
-//       clearTimeout(alarmTimeout);
-//       alert("Alarm cleared");
-//     }
-//     console.log("hello");
-//   }
-
-//   clearButton.addEventListener("click", () => {
-//     clearAlarm();
-//   });
-//   setButton.addEventListener("click", () => {
-//     setAlarm();
-//   });
-//   userDate.addEventListener("change", () => {
-//     setAlarmTime(userDate.value);
-//   });
-
-//   setInterval(updateTime, 1000);
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
-  const display = document.getElementById("clock");
-  const userDate = document.querySelector("input[type=datetime-local]");
-  const alarmButton = document.querySelector(".alarm__button");
+  const audio = new Audio(
+    "https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3"
+  );
+  const setButton = document.querySelector(".control__setButton");
+  const clearButton = document.querySelector(".control__clearButton");
+  const alarmInput = document.querySelector(".alarm__input");
+  const userMessage = document.querySelector(".alarm__text");
   const nextAlarm = document.querySelector("ul:first-of-type");
   const pastAlarm = document.querySelector("ul:not(ul:first-of-type)");
-  let timeoutId = [];
-  let i = 0;
+  let displayMessage = null;
+  audio.loop = true;
+  let alarmTime = null;
+  let alarmTimeout = null;
   let options = {
     year: "numeric",
     month: "long",
@@ -93,88 +22,74 @@ document.addEventListener("DOMContentLoaded", () => {
     weekday: "long",
   };
 
-  // TEST
-  // let date1 = new Date();
-  // let date2 = new Date(new Date().valueOf() + 1000 * 3600 * 24);
-
-  // if (date1.getTime() < date2.getTime()) {
-  //   alert("Correct");
-  // } else {
-  //   alert("Incorrect");
-  // }
-
-  // TEST
-
-  function setAlarm() {
-    let alarm = userDate.value;
-    // userDate.value = "";
-
-    if (alarm !== "") {
-      let current = new Date();
-      let userAlarm = new Date(alarm);
-      let duration = userAlarm - current;
-
-      //   if (duration < 0) {
-      //     // alert("Time has already passed");
-      //   } else {
-      // displayAlarm(
-      //   userAlarm.toLocaleDateString("fr-FR", {
-      //     year: "2-digit",
-      //     month: "2-digit",
-      //     day: "2-digit",
-      //     hour: "2-digit",
-      //     minute: "2-digit",
-      //     second: "2-digit",
-      //   })
-      // );
-      displayAlarm(userAlarm);
-
-      timeoutId[i++] = setTimeout(() => {
-        if (duration >= 0) {
-          alert("Reveil");
-        }
-      }, duration);
-      //   }
-    } else {
-      alert("Select Alarm Time !!!");
-    }
-  }
-
-  function displayAlarm(alarm) {
+  // Créé l'alarm set les listes !
+  function displayAlarm(newAlarm) {
+    // Besoin d'alarm time pour le "son".
+    alarmTime = alarmInput.value;
     let li = document.createElement("li");
-    let time = new Date(alarm);
-    let userAlarm = time.getTime();
+
+    // User alarm:
+    let time = newAlarm;
+    let userAlarm = new Date(time);
+    console.log(`USER : ${userAlarm}`);
+
+    // Current Time
     let current = new Date();
+    console.log(`ACTUEL : ${current}`);
 
-    let x = userAlarm - current;
-    let hours = formatTime(new Date(x).getHours() - 1);
-    let min = formatTime(new Date(x).getMinutes());
+    let sum = Date.parse(newAlarm) - Date.parse(current);
+    let date = new Date(sum).toLocaleTimeString("fr-FR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
 
-    if (userAlarm > current) {
-      // console.log("prochainement");
-      li.appendChild(
-        // document.createTextNode(`à ${hours}:${min}:${sec} dans x temps`)
-        document.createTextNode(
-          `le ${time.toLocaleString("fr-FR", options)} dans ${hours}h:${min}`
-        )
-      );
+    if (Date.parse(current) < Date.parse(newAlarm)) {
+      console.log("alarme à venir");
+
+      console.log(date);
+      li.innerHTML = `${userAlarm.toLocaleTimeString("fr-FR", options)} - ${
+        userMessage.value
+      }, dans ${date}.`;
       nextAlarm.appendChild(li);
     } else {
-      li.appendChild(
-        // document.createTextNode(`à ${hours}:${min}:${sec} passé`)
-        document.createTextNode(`le ${time.toLocaleString()} passé`)
-      );
+      console.log("alarme passée");
+      li.innerHTML = `${userAlarm.toLocaleTimeString("fr-FR", options)} - ${
+        userMessage.value
+      }, dans ${date}.`;
       pastAlarm.appendChild(li);
+    }
+
+    if (time) {
+      // const current = new Date();
+      // const timeToAlarm = new Date(alarmTime);
+      if (current.getTime() < userAlarm.getTime()) {
+        const timeout = userAlarm.getTime() - current.getTime();
+        let alarmTimeout = setTimeout(() => audio.play(), timeout);
+        let messageAlert = setTimeout(
+          () => window.alert(userMessage.value),
+          timeout
+        );
+        // window.alert("Reveil set up pour le" + " " + userMessage.value);
+      }
     }
   }
 
-  function clock() {
-    const date = new Date();
-    const hour = formatTime(date.getHours());
-    const minutes = formatTime(date.getMinutes());
-    const seconds = formatTime(date.getSeconds());
+  function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+      clearTimeout(alarmTimeout);
+      alert("Reveil cleared");
+    }
+  }
 
-    display.innerText = `${hour} : ${minutes} : ${seconds}`;
+  function timer() {
+    const date = new Date();
+    let display = document.querySelector(".display");
+    let hours = formatTime(date.getHours());
+    let min = formatTime(date.getMinutes());
+    let sec = formatTime(date.getSeconds());
+    let clock = hours + ":" + min + ":" + sec;
+    display.innerHTML = clock;
   }
 
   function formatTime(time) {
@@ -184,17 +99,20 @@ document.addEventListener("DOMContentLoaded", () => {
     return time;
   }
 
-  //  EVENT LISTENERS //
-  alarmButton.addEventListener("click", (e) => {
-    //Call setAlarm function
-    e.preventDefault();
-    setAlarm();
+  function cleanInput() {
+    return (userMessage.value = "");
+  }
+
+  /// EVENTS LISTENERS
+  setButton.addEventListener("click", () => {
+    displayAlarm(alarmInput.value);
+    cleanInput();
+    // setAlarme();
+    // console.log(alarmInput.value);
+  });
+  clearButton.addEventListener("click", () => {
+    clearAlarm();
   });
 
-  //handle delete alarm event for removing the li from thelist
-  //   ul.addEventListener("click", (e) => {
-  //     removeAlarm(e.target);
-  //   });
-
-  setInterval(clock, 1000);
+  setInterval(timer, 1000);
 });
